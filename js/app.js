ed94a7c607169977a25d11ef22b81ba999683f6e
acceptdead <acceptdead@gmail.com>
2014-12-05 16:59:24 +0800

    5

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/INTRO', {templateUrl: 'partials/INTRO.html', controller: GenericViewCtrl});
-    $routeProvider.when('/Project_WAF', {templateUrl: 'partials/project_a.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_iGAMEv', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
+    $routeProvider.when('/project_a', {templateUrl: 'partials/project_a.html', controller: GenericViewCtrl});
+    $routeProvider.when('/project_b', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
-    $routeProvider.otherwise({redirectTo: '/INTRO'});
+    $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: GenericViewCtrl});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);
