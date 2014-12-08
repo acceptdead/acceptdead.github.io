06e1b3d4a80b590443aaec6bba2f7da4d2dff8c3
acceptdead <acceptdead@gmail.com>
2014-12-08 08:24:45 +0800

    4
    4

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/intro', {templateUrl: 'partials/intro.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_a', {templateUrl: 'partials/project_a.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_b', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
+    $routeProvider.when('/project_1', {templateUrl: 'partials/project_waf.html', controller: GenericViewCtrl});
+    $routeProvider.when('/project_2', {templateUrl: 'partials/project_baas.html', controller: GenericViewCtrl});
     $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
-    $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: GenericViewCtrl});
+//    $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: //GenericViewCtrl});
     $routeProvider.otherwise({redirectTo: '/intro'});
   }]);
